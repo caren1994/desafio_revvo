0fc60337a4aa6ec5cceb2abea92424f32ad8b3b9
caren <carenoliveirasilvapontes@gmail.com>
2023-08-01 01:22:30 -0300

    feat:route método post,put,delete

--- front-end/app/api/cursos/route.js
+++ front-end/app/api/cursos/route.js
@@ -1,13 +1,50 @@
 import { NextResponse } from "next/server";
+
 export async function GET(){
     const request = await fetch("http://localhost:8000/?url=cursos");
     const response = await request.json(); 
     return NextResponse.json(response);
 
-}
+};
 
 export async function POST(request){
-    const body = await request.json();
-    console.log(body);
-    return NextResponse.json({ message: `Hello ${body.name}` });
-}
+    const {data} = await request.json();
+    const array= data.join("&");
+    const result = await fetch('http://localhost:8000/?url=cursos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },body:array
+      });
+    const response = await result.json(); 
+    return NextResponse.json(response);
+
+};
+
+export async function PUT(request){
+    const {data} = await request.json();
+    const array= data.join("&");
+    const result = await fetch('http://localhost:8000/?url=cursos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },body:array
+      });
+    const response = await result.json(); 
+    return NextResponse.json(response);
+
+};
+
+export async function DELETE(request){
+    const {data} = await request.json();
+    const array= data.join("&");
+    const result = await fetch('http://localhost:8000/?url=cursos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },body:array
+      });
+    const response = await result.json(); 
+    return NextResponse.json(response);
+
+};
