dda1c28cff961b6245d6d60c0e5e2b816e44afd0
caren <carenoliveirasilvapontes@gmail.com>
2023-08-01 01:22:08 -0300

    fix:nome variavel

--- front-end/app/api/cursos/[id]/route.js
+++ front-end/app/api/cursos/[id]/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 export async function GET(request,{params:{id}}){
-    const request = await fetch("http://localhost:8000/?url=cursos/"+id);
-    const response = await request.json(); 
+    const result = await fetch("http://localhost:8000/?url=cursos/"+id);
+    const response = await result.json(); 
     return NextResponse.json(response);
 
 }
