f7431fd734e909360cb706a1210d059ebd466d06
caren <carenoliveirasilvapontes@gmail.com>
2023-08-01 01:21:16 -0300

    fix:header mais compacto

--- front-end/components/Header.jsx
+++ front-end/components/Header.jsx
@@ -1,27 +1,18 @@
-const { useEffect,useState } = require("react");
-import Image from 'next/image';
-
+import style from '@/app/styles/header.module.css';
+import PerfilArea from '../components/PerfilArea';
+import Logo from '../components/Logo';
 
 export default function Header(){
-    const [users,setUsers] = useState([])
-    useEffect(()=>{
-        fetch('http://localhost:3000/api/users').then(res=>res.json()).then(data=>setUsers(data))
-    },[])
-    console.log(users)
+
     return(
         <>
-        {
-        users.map((user) => (
-            <div key={user.id} >
-                <p>Bem-vindo</p>
-                <p >{user.Nome}</p>
+      
+        <header className={ style.header }>
+          <Logo />
+          <PerfilArea />
+        </header>
+   
 
-                <Image src={user.Imagem} width={25} height={25} alt="mulher negra" />
-
-
-
-            </div>
-             ))}
 
 
         </>
