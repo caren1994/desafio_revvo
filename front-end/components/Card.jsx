e2b52c22512c745bd9b115e993e14157a0249c09
caren <carenoliveirasilvapontes@gmail.com>
2023-08-01 01:20:59 -0300

    feat:card adicionando mensagem em caso de erro e conclusão do post

--- front-end/components/Card.jsx
+++ front-end/components/Card.jsx
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import style from '@/app/styles/card.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function Card(){
+    const router = useRouter();
     const [cursos,setCursos] = useState([]);
     const [viewForm,setViewForm] = useState(false);
-    const [titulo,setTitulo] = useState('');
-    const [descricao,setDescricao] = useState('');
+    const [Titulo,setTitulo] = useState('');
+    const [Descricao,setDescricao] = useState('');
+    const [msnError,setMsnError] = useState({});
+    const Imagem = '/imagens/PC-setup.jpg';
+
     const handleCreate = ()=>{
-        const data = {
-            titulo,
-            descricao
-        };
-        fetch('http://localhost:3000/api/cursos',{
-            method:'POST',
-            headers:{
-                'Content-Type':'application/json'
+        fetch('http://localhost:3000/api/cursos', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
             },
-            body:JSON.stringify(data)
-        }).then((res)=>res.json()).then((data)=>{
-            console.log(data);
-            setTitulo('');
-            setDescricao('');
-            setViewForm(false);
-        });
+            body: JSON.stringify({data:[`Titulo=${Titulo}`,`Descricao=${Descricao}`,`Imagem=${Imagem}`]})
+          }).then((res)=>res.json()).then((data)=>{setMsnError(data)});
+          setTitulo('');
+          setDescricao('');
+          reload();
     }
+
 useEffect(()=>{
     fetch('http://localhost:3000/api/cursos').then(res=>res.json()).then(data=>setCursos(data))
 },[]);
@@ -31,6 +31,13 @@ const handleView =()=>{
     setViewForm(!viewForm);
 
 };
+const viewCursoId = (id) => {
+    console.log(id)
+  router.push('/cursos/'+id);
+  };
+  const reload = ()=>{
+    fetch('http://localhost:3000/api/cursos').then(res=>res.json()).then(data=>setCursos(data))
+  };
 
 return(
     <>
@@ -41,7 +48,7 @@ return(
                     <img src={curso.Imagem} style={{width:130,height:70}} alt="Imagem do curso"/>
                     <p>{curso.Titulo}</p>
                     <p>{curso.Descricao}</p>
-                    <button type='button'>Ver Curso</button>
+                    <button type='button' onClick={()=>viewCursoId(curso.id)}>Ver Curso</button>
 
                 </div>
             ))
@@ -52,6 +59,7 @@ return(
             </button>
         </div>
     </section>
+    <div className={style.divForm}>
         {
         viewForm && <form className={style.form}>
             <label htmlFor="titulo">Título</label>
@@ -59,18 +67,23 @@ return(
             id="titulo" 
             name="titulo" 
             placeholder="Título do curso" 
-            value={titulo}
+            value={Titulo}
 			onChange={({ target }) => setTitulo(target.value)} required/>
             <label htmlFor="descricao">Descrição</label>
             <input type="text" 
             id="descricao" 
             name="descricao" 
             placeholder="Descrição do curso" 
-            value={descricao}
+            value={Descricao}
 			onChange={({ target }) => setDescricao(target.value)} required/>
-            <button type="button" onClick={handleCreate}>Criar Curso</button>
+            <div >
+            <button type="button"  onClick={handleCreate}>Criar Curso</button>
+            <button type="button" onClick={()=>setViewForm(false)}>X</button>
+            </div>
+            {msnError && <p style={{color:'red'}}>{msnError.message}</p>}
             </form>
         }
+        </div>
         </>
 );
 }
